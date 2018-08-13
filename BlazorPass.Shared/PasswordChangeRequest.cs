b7e8b07b47ccb2737f6548b52a1c738262233254
Eugene Bekker <ebekker@users.noreply.github.com>
2018-08-12 20:10:52 -0400

    mised the shared

---
+++ BlazorPass.Shared/PasswordChangeRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPass.Shared
+{
+    public class PasswordChangeRequest
+    {
+        public string Username { get; set; }
+
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
---
+++ BlazorPass.Shared/PasswordChangeResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPass.Shared
+{
+    public class PasswordChangeResponse
+    {
+        public int ErrorCode { get; set; }
+
+        public string ErrorName { get; set; }
+
+        public string ErrorDetail { get; set; }
+    }
+}
